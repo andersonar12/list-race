3b375ffe9fcc3d4f0cc81a66afe1c51688d32370
Anderson Romero <62034538+andersonar12@users.noreply.github.com>
2024-06-10 01:42:44 -0400

    improvements on Alert

--- src/components/Alert.tsx
+++ src/components/Alert.tsx
@@ -26,7 +26,17 @@ export const Alert = ({ result, successMessage, errorMessage, open, setOpen }: A
       ) : result.length > 0 ? (
         <div>
           <h3>{successMessage}</h3>
-          <pre>{JSON.stringify(result, null, 2)}</pre>
+          {/* <pre>{JSON.stringify(result, null, 2)}</pre> */}
+          <ul className="w-full text-sm font-medium  bg-white border border-gray-200 rounded-lg dark:bg-gray-700 dark:border-gray-600 dark:text-white">
+            {result.map((item) => (
+              <li
+                key={item.id}
+                className="w-full px-4 py-2 border-b border-gray-200 rounded-t-lg dark:border-gray-600"
+              >
+                <p className="text-white">{item.name}</p>
+              </li>
+            ))}
+          </ul>
         </div>
       ) : (
         <h3>{errorMessage}</h3>
